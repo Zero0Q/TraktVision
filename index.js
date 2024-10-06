63cb4e1c227d07ab6e3380af2f375aea2b3ff508
qooode <71751652+qooode@users.noreply.github.com>
2024-10-06 18:43:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const addon = new addonBuilder({
     version: '1.0.0',
     name: 'TraktVision',
     description: 'Visualize your Trakt watching progress on Cinemeta posters',
-    resources: ['catalog', 'meta', 'stream'],
+    resources: ['catalog', 'meta', 'stream', 'poster'], // Add 'poster' to the resources array
     types: ['movie', 'series'],
     catalogs: [],
     idPrefixes: ['tt']
